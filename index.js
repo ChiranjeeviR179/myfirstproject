b71b495761219216778a065755dfdee857f5378a
Chiranjeevi <rchiranjeevi179@outlook.com>
2018-11-21 17:35:25 +0530

    intro 1 test again

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ const server = http.createServer((req,res) => {
 
 })
 
-server.listen(port,() => {
+server.listen(process.env.PORT || port,() => {
     console.log(`Server running at : ${hostname}:${port}`);
 })
