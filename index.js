1630073d0ef17317801a90be804af6e0c282e063
Chiranjeevi <rchiranjeevi179@outlook.com>
2018-11-21 17:20:05 +0530

    intro 2

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
-// var rect = 
-// {
-//     perimeter : (x,y) => (2*(x+y)) ,
-//     area : (x,y) => (x*y)
-// };
+//console.log("hello world");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-var rect = require('./rectangle');
+const hostname = 'localhost';
+const port = 3000;
 
-function calcrect(l,b)
-{
+const server = http.createServer((req,res) => {
 
-   console.log("the length : "+l+" the breath : "+b); 
+    //console.log(req.headers);
 
-// if( l <= 0 || b <= 0)
-// {
-//     console.log("the length and breath should be greather then 0");
-// }
-// else
-// {
-// console.log("perimeter : "+rect.perimeter(l,b));
-// console.log("area : "+rect.area(l,b));
-// }
-rect(l,b,(err, rectangle) => {
-if(err)
-{
-    console.log("error : "+err.message);
-}
-else
-{
-    console.log("perimeter : "+rectangle.perimeter());
-    console.log("area : "+rectangle.area());
-}
 
-});
+    console.log("Request for" +req.url+" by methode " +req.method);
 
-console.log("Next value");
-}
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.end('<html><body> <h1> Hello, World</h1></body></html>')
 
-calcrect(10,4);
-calcrect(0,3);
-calcrect(-1,2);
+
+
+    if(req.method == 'GET')
+    {
+        var fileUrl;
+
+        if(req.url == '/') fileUrl ='/index.html';
+        else fileUrl = req.url; 
+
+        var filePath = path.resolve('./public'+fileUrl);
+
+        const fileExt = path.extname(filePath);
+
+        if(fileExt == '.html')
+        {
+            fs.exists(filePath, (exists) => {
+
+                if(!exists)
+                {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end("<html><body><h1> the file "+fileUrl+" not found, statuscode 404</h1></body></html>");
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type','text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text/html');
+            res.end("<html><body><h1> the file url is not html file, statuscode 404</h1></body></html>");
+            return;
+
+        }
+
+    }
+    else
+    {
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html');
+        res.end("<html><body><h1> the method is not get, statuscode 404</h1></body></html>");
+        return;
+    }
+
+})
+
+server.listen(port,hostname,() => {
+    console.log(`Server running at : ${hostname}:${port}`);
+})
